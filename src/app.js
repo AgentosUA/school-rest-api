53189ff1895092e8e91ab41c5024fdb23386da94
Agentos <agentoswork@gmail.com>
2020-06-24 22:30:21 +0300

    fix: path to .config file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: './src/.config'});
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
