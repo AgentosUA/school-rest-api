1819637cf09dc58ff55daaeafa04673fff2fb6fc
Agentos <agentoswork@gmail.com>
2020-06-25 17:12:07 +0300

    add getAllTeachers & postNewTeacher methods

--- src/controllers/teachers.js
+++ src/controllers/teachers.js
@@ -1,12 +1,58 @@
-exports.getAllTeachers = (req, res) => {
+const mongoose = require('mongoose');
+const Teacher = require('../models/teacher');
+
+const { validationResult } = require('express-validator');
+
+exports.getAllTeachers = async (req, res) => {
+  try {
+    const teachers = await Teacher.find();
+    if (teachers.length < 1) {
+      return res.status(404).json({
+        error: 'No teachers found',
+        status: 404,
+      });
+    }
     res.status(200).json({
-        data: [{
-            name: 'John',
-            age: 24,
-        }, {
-            name: 'Grims',
-            age: 35,
-        }],
-        status: 200
-    })
-}
+      teachers,
+      error: true,
+      status: 404,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 'Something went wront on the server!',
+      status: 500,
+    });
+  }
+};
+
+exports.postNewTeacher = async (req, res) => {
+  const { name } = req.body;
+  const validErrors = validationResult(req);
+
+  if (!validErrors.isEmpty()) {
+    return res.status(400).json({
+      error: 'Some fields are empty!',
+      status: 400,
+    });
+  }
+
+  const teacher = new Teacher({
+    name,
+    subjects: [],
+  });
+
+  try {
+    await teacher.save();
+    res.status(200).json({
+      message: 'Teacher created successfully!',
+      error: false,
+      status: 200,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Something went wront on the server!',
+      status: 500,
+    });
+  }
+};
