0edd711cc4cf929bfc76052b2533d7ad76a3882e
Agentos <agentoswork@gmail.com>
2020-06-25 17:10:48 +0300

    add teachers post route & validation

--- src/routes/teachers/teachers.js
+++ src/routes/teachers/teachers.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 const teachersController = require('../../controllers/teachers');
 
 router.get('/', teachersController.getAllTeachers);
+router.post('/', [body('name').notEmpty()],teachersController.postNewTeacher);
 
 module.exports = router;
