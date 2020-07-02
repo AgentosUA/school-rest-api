e904ba17d0900c9cc73b3b0f108451b4a913687b
Agentos <agentoswork@gmail.com>
2020-07-02 20:17:47 +0300

    add: patchTeacher method and route

--- src/controllers/teachers.js
+++ src/controllers/teachers.js
@@ -29,6 +29,36 @@ exports.getTeacher = async (req, res) => {
   }
 };
 
+exports.patchTeacher = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const teacher = await Teacher.findById(
+      mongoose.Types.ObjectId(req.params.id)
+    );
+    if (!teacher) {
+      return res.status(404).json({
+        error: 'Teacher not found',
+        status: 404,
+      });
+    }
+
+    teacher.name = name;
+    await teacher.save();
+
+    res.status(200).json({
+      message: 'Teacher updated successfully',
+      error: false,
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 'Something went wrong on the server!',
+      status: 500,
+    });
+  }
+};
+
 exports.getAllTeachers = async (req, res) => {
   try {
     const teachers = await Teacher.find();
--- src/routes/teachers/teachers.js
+++ src/routes/teachers/teachers.js
@@ -5,6 +5,7 @@ const teachersController = require('../../controllers/teachers');
 
 router.get('/', teachersController.getAllTeachers);
 router.get('/:id', teachersController.getTeacher);
+router.patch('/:id', teachersController.patchTeacher);
 router.post('/', [body('name').notEmpty()],teachersController.postNewTeacher);
 router.delete('/:id', teachersController.deleteTeacher);
 
